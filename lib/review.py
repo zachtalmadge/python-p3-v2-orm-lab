e386783480711580f557494b427fe26086eef6b2
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-10-28 11:34:01 -0400

    pass some

--- lib/review.py
+++ lib/review.py
@@ -19,6 +19,37 @@ class Review:
             f"<Review {self.id}: {self.year}, {self.summary}, "
             + f"Employee: {self.employee_id}>"
         )
+        
+    @property
+    def year(self):
+        return self._year
+    
+    @year.setter
+    def year(self, new_year):
+        if isinstance(new_year, int) and new_year >= 2000:
+            self._year = new_year
+        else:
+            raise Exception('Year must be after 2000!')
+        
+    @property
+    def summary(self):
+        return self._summary
+    
+    @summary.setter
+    def summary(self, new_summary):
+        if new_summary == '':
+            raise Exception('Cant be an empty string!')
+        else:
+            self._summary = new_summary
+    
+    @property
+    def employee_id(self):
+        return self._employee_id
+    
+    @employee_id.setter
+    def employee_id(self, new_employee):
+        pass
+        
 
     @classmethod
     def create_table(cls):
@@ -47,33 +78,90 @@ class Review:
         """ Insert a new row with the year, summary, and employee id values of the current Review object.
         Update object id attribute using the primary key value of new row.
         Save the object in local dictionary using table row's PK as dictionary key"""
-        pass
+        
+        sql = """
+            INSERT INTO reviews (year, summary, employee_id)
+            VALUES (?, ?, ?)
+        """
+        
+        CURSOR.execute(sql, (self.year, self.summary, self.employee_id))
+        CONN.commit()
+        
+        self.id = CURSOR.lastrowid
+        type(self).all[self.id] = self
 
     @classmethod
     def create(cls, year, summary, employee_id):
         """ Initialize a new Review instance and save the object to the database. Return the new instance. """
-        pass
-   
+        
+        review = cls(year, summary, employee_id)
+        review.save()
+        return review
+    
     @classmethod
     def instance_from_db(cls, row):
         """Return an Review instance having the attribute values from the table row."""
+        
         # Check the dictionary for  existing instance using the row's primary key
-        pass
+        review = cls.all.get(row[0])
+        
+        if review:
+            # ensure attributes match row values in case local instance was modifed
+            review.year = row[1]
+            review.summary = row[2]
+            review.employee_id = row[3]
+            
+        else:
+            # if not in dictionary, create new instance and add to dictionary
+            review = cls(row[1], row[2], row[3])
+            review.id = row[0]
+            cls.all[review.id] = Employee
+            
+        return review
    
 
     @classmethod
     def find_by_id(cls, id):
         """Return a Review instance having the attribute values from the table row."""
-        pass
+        
+        sql = """
+            SELECT *
+            FROM reviews
+            WHERE id = ?
+        """
+        
+        row = CURSOR.execute(sql, (id,)).fetchone()
+        return cls.instance_from_db(row) if row else None
 
     def update(self):
         """Update the table row corresponding to the current Review instance."""
-        pass
+        
+        sql = """
+            UPDATE reviews
+            SET year = ?, summary = ?, employee_id = ?
+            WHERE id = ?
+        """
+        
+        CURSOR.execute(sql, (self.year, self.summary, self.employee_id, self.id))
+        CONN.commit()
 
     def delete(self):
         """Delete the table row corresponding to the current Review instance,
         delete the dictionary entry, and reassign id attribute"""
-        pass
+        
+        sql = """
+            DELETE FROM reviews
+            WHERE id = ?
+        """
+        
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
+        
+        # delete the dictionary entry using the id as the key
+        del type(self).all[self.id]
+        
+        # set the id to None
+        self.id = None
 
     @classmethod
     def get_all(cls):
